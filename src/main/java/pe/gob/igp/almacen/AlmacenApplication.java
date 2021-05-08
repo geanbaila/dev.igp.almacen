46e6a6215622c7dc6582fc5c53c6a5340e0b29e6
geanbaila <geanbaila@gmail.com>
2021-05-08 13:56:56 -0500

    database created

--- src/main/java/pe/gob/igp/almacen/AlmacenApplication.java
+++ src/main/java/pe/gob/igp/almacen/AlmacenApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IgpInventoryApplication {
+public class AlmacenApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IgpInventoryApplication.class, args);
+		SpringApplication.run(AlmacenApplication.class, args);
 	}
 
 }
